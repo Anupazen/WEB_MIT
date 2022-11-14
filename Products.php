587405858991d0d7c3b17f13b21da2f10bf302ea
Anupa Senevirathne <anupasenevirathne@gmail.com>
2022-11-14 21:40:25 +0530

    commit 2022.11.14

--- Products.php
+++ Products.php
@@ -1,4 +1,18 @@
 <html>
+<!-- <?php
+// Start the session
+//session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+//$_SESSION["favcolor"] = "green";
+//$_SESSION["favanimal"] = "cat";
+//echo "Session variables are set.";
+?> -->
 
 <?php
 	require 'DB.php';
@@ -10,7 +24,7 @@
 
     <head>
     <link rel="stylesheet" href="CSS/bootstrap.css" media="screen">
-    <link rel="stylesheet" href="CSS/Products.css">
+    
     </head>
     <body>
                             <?php
@@ -24,60 +38,12 @@
                                             <center>
                                             <h5 class="card-title"><?php echo $rows["Name"] ?></h5>
                                             <p class="card-text"><?php echo $rows["Size"] ?> / Unit</p>
-                                            <p style="color:green" class="card-text">Rs. <?php echo $rows["Price"] ?>/=</p>
-
-                                            <div class="cont">
-   <div class="crtdiv">
-      <span class="qty">
-      <span class="dec">
-      <i class="fa fa-minus-square" aria-hidden="true"></i>
-      </span>
-      <span class="num">
-      1
-      </span>
-      <span class="inc">
-      <i class="fa fa-plus-square" aria-hidden="true"></i>
-      </span>
-      </span>
-      <button id="btn" type="button" class="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</button>
-   </div>
-</div>
-                                            
+                                            <p style="color:green" class="card-text">Rs. <?php echo $rows["Price"] ?>/=</p>   
+                                            <button>Add To Cart</button>                                        
                                             </center>
                                         </div>
                                         </div>
                                         <br><br>
-
-                                     <script>
-                                        $(document).ready(function() {
-    $('#btn').click(function() {
-        $('#btn').toggleClass("cart_clk");
-
-    });
-    $("#btn").one("click", function() {
-        $('.cart .fa').attr('data-before', '1');
-    });
-
-    var prnum = $('.num').text();
-    $('.inc').click(function() {
-        if (prnum > 0) {
-            prnum++;
-            $('.num').text(prnum);
-            $('.cart .fa').attr('data-before', prnum);
-        }
-
-    });
-    $('.dec').click(function() {
-        if (prnum > 1) {
-            prnum--;
-            $('.num').text(prnum);
-            $('.cart .fa').attr('data-before', prnum);
-        }
-
-    });
-
-});
-                                     </script>   
                             <?php
 									}
 							?>
