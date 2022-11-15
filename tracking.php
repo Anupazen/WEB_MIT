ed8305b07c2922556204cb7d568e6db28352af2b
mihisurup <88051236+mihisurup@users.noreply.github.com>
2022-11-15 23:13:15 +0530

    Footer style

---
+++ tracking.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/trackingStyle.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Tracking Info</h2> 
+
+        <div class="tracker">
+            <ul class="step-wizard-list">
+                <li class="step-wizard-item">
+                    <span class="progress-count">1</span>
+                    <span class="progress-label">Ordered</span>
+                </li>
+                <li class="step-wizard-item current-item">
+                    <span class="progress-count">2</span>
+                    <span class="progress-label">Packed the Order</span>
+                </li>
+                <li class="step-wizard-item">
+                    <span class="progress-count">3</span>
+                    <span class="progress-label">Shipped</span>
+                </li>
+                <li class="step-wizard-item">
+                    <span class="progress-count">4</span>
+                    <span class="progress-label">Order Completed</span>
+                </li>
+            </ul>
+        </div>
+
+        <div class="container2">
+            <div class="delcont">
+            
+                <div class="delinfo">
+                    <h5>Delivery Adress</h5>
+                </div>
+            </div>
+            <div class="paycont">
+            
+                <div class="payinfo">
+                    <h5>Payment Info</h5>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    
+</body>
+</html>
